5a32d6bb457e93ee14a10936642017717ea5e837
Nata-Kalina <kalinichevanata888@gmail.com>
2024-02-01 22:26:18 -0500

    Associations and validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,15 @@
 class Company < ApplicationRecord
+
+    validates :company_title, presence: true
+    validates :about_company, presence: true
+    validates :company_industry, presence: true
+    validates :company_website, presence: true
+    validates :company_location, presence: true
+    validates :company_email, email: true
+    validates :company_phone_number, presence: true, numericality: { only_integer: true }, length: { is: 10 }
+    validates :company_linkedin_profile, presence: true
+    
+    validates_associated :user
+
+    has_many :jobs, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, format: { with: /\A[a-z\-' ]+\z/i }, length: { maximum: 255 }, length: { mimimum: 2 }
+    validates :last_name, presence: true, format: { with: /\A[a-z\-' ]+\z/i }, length: { maximum: 255 }, length: { mimimum: 2 }
+    validates :user_phone_number, presence: true
+    validates :user_phone_number, numericality: { only_integer: true }
+    validates :user_phone_number, length: { is: 10 }
+    validates :user_email, presence: true, email: true, uniqueness: true
+    validates :user_password, password_strength: true
+    validates :user_role, presence: true, inclusion: { in: %w[jobseeker employer admin] }
+
+    has_many :applications, dependent: :destroy
 end
